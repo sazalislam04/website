a37a972d865fe274e6fb3ca777d044442ad4183b
sazalislam04 <mdsazalislam04@gmail.com>
2023-02-05 11:29:07 +0600

    first commit

--- components/JobCard.js
+++ components/JobCard.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import {Col, Row} from 'react-bootstrap'
-import { useRouter } from 'next/router';
-import { IoIosArrowForward } from 'react-icons/io';
+import { useRouter } from "next/router";
+import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
 
-export default function JobCard({data}) {
+export default function JobCard({ data }) {
+  const { skill, skillLogo, slug } = data.fields;
+  const router = useRouter();
+  const ClickHandler = () => {
+    router.push(`/jobs/${slug}`);
+  };
 
-    const {skill, skillLogo, slug} = data.fields
-    const router = useRouter()
-    const ClickHandler = () => {
-        router.push(`/jobs/${slug}`)
-    }
-
-  return(
-
-      <div className='technology_colam' onClick={ClickHandler} lg={4} md={6} sm={12}>
-          <img className='hire_tech_img' src={'https:' + skillLogo.fields.file.url}  alt={skill} />
-          <p className="technology_text">{skill}</p>
-          <IoIosArrowForward className="hire_arrow_icon ms-2"/>
-          {/* <p className="category_text">{category.fields.categoryName}</p> */}
-      </div>
-  )
+  return (
+    <div
+      className="technology_colam"
+      onClick={ClickHandler}
+      lg={4}
+      md={6}
+      sm={12}
+    >
+      <img
+        className="hire_tech_img"
+        src={"https:" + skillLogo.fields.file.url}
+        alt={skill}
+      />
+      <p className="technology_text">{skill}</p>
+      <IoIosArrowForward className="hire_arrow_icon ms-2" />
+      {/* <p className="category_text">{category.fields.categoryName}</p> */}
+    </div>
+  );
 }
-{/* <img src={'https:' + thumbnail.fields.file.url} width="100%" height="200px" alt='' /> */}
+{
+  /* <img src={'https:' + thumbnail.fields.file.url} width="100%" height="200px" alt='' /> */
+}
